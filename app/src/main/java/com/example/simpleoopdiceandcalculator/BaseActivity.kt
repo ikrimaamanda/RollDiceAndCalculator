a2037d07d62c4a5dd254cdaccba2fbb5ad31e63a
Ikrima Amanda <ikrima.amanda.07@gmail.com>
2020-12-14 05:50:20 +0800

    update file

--- app/src/main/java/com/example/simpleoopdiceandcalculator/BaseActivity.kt
+++ app/src/main/java/com/example/simpleoopdiceandcalculator/BaseActivity.kt
@@ -1,14 +1,8 @@
 package com.example.simpleoopdiceandcalculator
 
-import android.os.Bundle
-import android.view.View
-import androidx.annotation.Nullable
 import androidx.appcompat.app.AppCompatActivity
 
 abstract class BaseActivity: AppCompatActivity() {
-//    override fun onCreate(@Nullable savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//    }
 
     open fun initializationListeners() {}
 
